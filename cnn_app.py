5301426ef88eb17214c310108e33da8e73035794
Ben chelbi Wissem <Ben_chelbiw@yahoo.com>
2021-11-30 22:35:24 +0100

    ra

--- cnn_app.py
+++ cnn_app.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageOps
 import tensorflow as tf
 import pandas as pd
 
-model = tf.keras.models.load_model('MyModel_h5.h5')
+model = tf.keras.models.load_model('model_pourri2.h5')
 
 def import_and_predict(image_data, model):
     
@@ -22,7 +22,7 @@ def import_and_predict(image_data, model):
         
         return res
 
-model = tf.keras.models.load_model('MyModel_h5.h5')
+model = tf.keras.models.load_model('model_pourri2.h5')
 
 st.write("""
          # Letter prédiction
